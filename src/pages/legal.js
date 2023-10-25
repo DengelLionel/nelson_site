d40ab9eb47048d508d9c4d5efc0508516d427893
DengelLionel <dengelriveracajincho@gmail.com>
2023-10-24 19:02:37 -0500

    df

--- src/pages/legal.js
+++ src/pages/legal.js
@@ -1,14 +1,14 @@
 import React from 'react'
 import AppLayout from '@/components/Layouts/AppLayout'
 import Head from 'next/head'
-import Lista from '@/components/representantelegal/Lista'
+/* import Lista from '@/components/representantelegal/Lista'
 import axios from '@/lib/axios'
-import useSWR from 'swr'
+import useSWR from 'swr' */
 const legal = () => {
-    const data = useSWR('/api/datoslegal', () =>
+/*     const data = useSWR('/api/datoslegal', () =>
         axios.get('/api/datoslegal').then(res => res.data),
     )
-    const datos = data.data
+    const datos = data.data */
     return (
         <AppLayout
             header={
